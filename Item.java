a84f2b4af4db1fd0096ee03de24a815a4a7cd693
Doolyxo <abdulqshaik1@gmail.com>
2024-11-14 17:59:03 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,4 +1,4 @@
-public class Item { // new
+public class Item {
     private String name;
     private String description;
     private boolean isUsable;
@@ -7,39 +7,29 @@ public class Item { // new
         this.name = name;
         this.description = description;
         this.isUsable = isUsable;
-
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
+    public String getDescription() {
+        return description;
+    }
 
     public void use() {
-        if (isUsable){
-            System.out.println("You used the " + name + ".");
-            // You can add item specific logic here if needed
-
+        if (isUsable) {
+            switch (name.toLowerCase()) {
+                case "crown of foresight":
+                    System.out.println("You feel a wave of clarity wash over you as visions of possible futures unfold.");
+                    // Additional logic specific to the Crown of Foresight can go here
+                    break;
+                default:
+                    System.out.println("You used the " + name + ".");
+                    // Add any default item usage logic here if needed
+            }
         } else {
             System.out.println("This item cannot be used.");
-
         }
-    
-       //public void use() { // new might need to improvements
-       //    if (isUsable) {
-       //         switch (name.toLowerCase()) {
-       //             case "crown of foresight":
-        //                System.out.println("You feel a wave of clarity wash over you as visions of possible futures unfold.");
-                        // Add logic specific to this item effect
-        //                break;
-         //           default:
-        //                System.out.println("You used the " + name + ".");
-        //        }
-         //   } else {
-        //        System.out.println("This item cannot be used.");
-       //     } (new might need to improvements)
-            
     }
 }
-
-
