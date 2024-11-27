85e927e15ed439924e6691e0aeaa273f681d9d67
Ishnol <ekizzie1@students.towson.edu>
2024-11-26 22:54:45 -0500

    Update Everything.java

--- Everything.java
+++ Everything.java
@@ -72,3 +72,42 @@ public class Character {
     }
 }
 // Character section finish
+
+// entity section start
+
+public class Entity {
+    private String name;
+    private int health;
+    private int attackPower;
+
+    public Entity(String name, int health, int attackPower){
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+    
+    public void takeDamage(int damage){
+        health -= damage;
+        if (health <= 0){
+            System.out.println(name + " has been defeated");
+
+        }
+    }
+
+    public int attack()  {
+        System.out.println(name + " attakcs with power " + attackPower + "!" );
+        return attackPower;
+    }
+}
+
+// entity section finish
+
+
